678291fe0912bb600a50d04078fb8c16150a7588
unknown <pawelpluta58@gmail.com>
2020-04-08 14:13:05 +0200

    PP 2020-04-08

--- scraper.py
+++ scraper.py
@@ -26,6 +26,13 @@ tags = {
     "purchased": ["div", "product-review-pz", "em"]
 }
 
+#funkcja do usuwania znaków formatujących
+def remove_whitespaces(string):
+    try:
+        return string.replace("\n", ", ").replace("\r", ", ")
+    except AttributeError:
+        pass
+
 #zdefiniowanie URL strony z opiniami
 url_prefix = "https://www.ceneo.pl"
 product_id = input("Podaj kod produktu: ")
@@ -41,15 +48,19 @@ while url:
     page_tree = BeautifulSoup(page_respons.text, 'html.parser')
 
     #wydobicie z kodu HTML strony fregmentów odpowiadających poszczegolnych opiniom
-    opinions = page_tree.find_all("li", "review-box")
+    opinions = page_tree.find_all("li", "js_product-review")
 
     #wydobycie składowych dla pojedynczej opinii
     for opinion in opinions:
         features = {key:extract_feature(opinion, *args)
                     for key, args in tags.items()}
-
         features["purchased"] = (features["purchased"] == "Opinia potwierdzona zakupem")
-        features["opinion_id"] = opinion["data-entry-id"]
+        features["opinion_id"] = int(opinion["data-entry-id"])
+        features["useful"] = int(features["useful"])
+        features["useless"] = int(features["useless"])
+        features["content"] = remove_whitespaces(features["content"])
+        features["pros"] = remove_whitespaces(features["pros"])
+        features["cons"] = remove_whitespaces(features["cons"])
         dates = opinion.find("span", "review-time").find_all("time")
         features["review_date"] = dates.pop(0)["datetime"]
         try:
@@ -67,6 +78,5 @@ while url:
 
 with open("./opinions_json/"+product_id+'.json', 'w', encoding='utf-8') as fp:
     json.dump(opinions_list, fp, ensure_ascii=False, indent=4, separators=(',', ': '))
-
 
 # pprint.pprint(opinions_list)

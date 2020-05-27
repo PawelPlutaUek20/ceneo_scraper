9115513c35c8f6b51d107880042d875e28482118
unknown <pawelpluta58@gmail.com>
2020-05-27 14:50:18 +0200

    PP 2020-05-27

--- app/analyzer.py
+++ app/analyzer.py
@@ -11,36 +11,34 @@ print(*os.listdir("./opinions_json"))
 product_id = input("Podaj kod produktu: ")
 
 #wczytanie do ramki danych opinii z pliku
-opinions = pd.read_json("./opinions_json/" + product_id + ".json")
-opinions = opinions.set_index("opinion_id")
-
-opinions["stars"] = opinions["stars"].map(lambda x: float(x.split("/")[0].replace(",", ".")))
+opinions = pd.read_json("./opinions_json/"+product_id+'.json')  
+opinions = opinions.set_index("opinion_id") 
 
 #częstość występowania poszczególnej liczby gwiazdek
-stars = opinions["stars"].value_counts().sort_index().reindex(list(np.arange(0, 5.1, 0.5)), fill_value = 0)
-fix, ax = plt.subplots()
+stars = opinions["stars"].value_counts().sort_index().reindex(list(np.arange(0, 5.1, 0.5)), fill_value=0)
+fig, ax = plt.subplots()
 stars.plot.bar(color="deepskyblue")
 plt.xticks(rotation=0)
 ax.set_title("Częstość występowania poszczególnych ocen")
 ax.set_xlabel("liczba gwiazdek")
 ax.set_ylabel("liczba opinii")
-plt.savefig("./figures_png/"+product_id+"_bar.png")
+plt.savefig("./figures_png/"+product_id+'_bar.png')
 plt.close()
 
-#udział poszczególnych rekomendacji w ogólnej liczbie opinii
+#udział poszczególnych rekomandacji w ogólnej liczbie opinii
 recommendation = opinions["recommendation"].value_counts()
-fix, ax = plt.subplots()
-recommendation.plot.pie(label="", autopct="%.1f%%", colors=["mediumseagreen", "coral"])
-ax.set_title("Udział poszczególnych rekomendacji w ogólnej liczbie opinii")
-plt.savefig("./figures_png/"+product_id+"_pie.png")
+fig, ax = plt.subplots()
+recommendation.plot.pie(label="", autopct="%.1f%%", colors=['mediumseagreen', 'coral'])
+ax.set_title("Udział poszczególnych rekomandacji w ogólnej liczbie opinii")
+plt.savefig("./figures_png/"+product_id+'_pie.png')
 plt.close()
 
-#podstawowe statystyki
-stars_average = opinions["stars"].mean()
+#podstawowe startstyki
+stars_everage = opinions["stars"].mean()
 pros = opinions["pros"].count()
 cons = opinions["cons"].count()
 purchased = opinions["purchased"].sum()
-print(stars_average, pros, cons, purchased)
+print(stars_everage, pros, cons, purchased)
 
-stars_purchased = pd.crosstab(opinions["stars"], opinions["purchased"])
+stars_purchased = pd.crosstab(opinions["stars"],opinions["purchased"])
 print(stars_purchased)
--- app/views.py
+++ app/views.py
@@ -1,10 +1,10 @@
-#import bibliotek
 from app import app
 from flask import render_template, request, redirect, url_for
 from flaskext.markdown import Markdown
 from app.forms import ProductForm
 from app.models import Opinion, Product
 import requests
+import pandas as pd
 Markdown(app)
 
 app.config['SECRET_KEY'] = "TajemniczyMysiSprzęt"
@@ -30,13 +30,30 @@ def extract():
         if page_respons.status_code == requests.codes['ok']:
             product = Product(product_id)
             product.extract_product()
+            product.save_product()
             return redirect(url_for("product", id=product_id))
         else:
-            form.product_id.errors.append("Podana wartość nie jest poprawnym kodem produktu")
+            form.product_id.errors.append("Podana wartość nie jest poprawnym kodem ptoduktu.")
     return render_template("extract.html", form=form)
 
-@app.route("/product/<id>")
+@app.route('/product/<id>')
 def product(id):
+    product = Product(id)
+    product.read_product()
+    opinions = pd.DataFrame.from_records([opinion.__dict__() for opinion in product.opinions])
+    opinions["stars"] = opinions["stars"].map(lambda x: float(x.split("/")[0].replace(",", ".")))
+    return render_template(
+        "product.html",
+        tables=[
+            opinions.to_html(
+                classes='table table-bordered table-sm table-responsive',
+                table_id = "opinions",
+                index = False
+            )
+        ], 
+        titles=opinions.columns.values 
+    )
+
     pass
 
 @app.route('/products')
